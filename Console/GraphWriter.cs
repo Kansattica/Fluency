d90c53987d463aed223123f27488be541819f822
Grace <grace@hypnovir.us>
2019-07-23 21:19:02 -0700

    Put GraphWriter in a proper namespace.

--- Console/GraphWriter.cs
+++ Console/GraphWriter.cs
@@ -4,120 +4,123 @@ using System.Xml;
 using System.Xml.Serialization;
 using Fluency.Interpreter.Parser.Entities.FunctionGraph;
 
-public class GraphWriter
+namespace Fluency.CLI
 {
-    public struct Graph
+    public class GraphWriter
     {
-        public Node[] Nodes;
-        public Link[] Links;
-    }
-
-    public struct Node
-    {
-        [XmlAttribute]
-        public string Id;
-        [XmlAttribute]
-        public string Label;
-
-        public Node(string id, string label)
+        public struct Graph
         {
-            this.Id = id;
-            this.Label = label;
-        }
-    }
-
-    public struct Link
-    {
-        [XmlAttribute]
-        public string Source;
-        [XmlAttribute]
-        public string Target;
-        [XmlAttribute]
-        public string Label;
-
-        public Link(string source, string target, string label)
-        {
-            this.Source = source;
-            this.Target = target;
-            this.Label = label;
-        }
-    }
-
-    public List<Node> Nodes { get; protected set; }
-    public List<Link> Links { get; protected set; }
-
-    public GraphWriter()
-    {
-        Nodes = new List<Node>();
-        Links = new List<Link>();
-    }
-
-    public void AddNode(Node n)
-    {
-        this.Nodes.Add(n);
-    }
-
-    public void AddLink(Link l)
-    {
-        this.Links.Add(l);
-    }
-
-
-    private HashSet<int> _seen = new HashSet<int>();
-    public void WalkFunctionGraph(FunctionNode head)
-    {
-        if (!_seen.Contains(head.Id))
-        {
-            AddNode(head);
-            _seen.Add(head.Id);
-        }
-        else
-        {
-            return;
+            public Node[] Nodes;
+            public Link[] Links;
         }
 
-        if (head.TopOut != null)
+        public struct Node
         {
-            WalkFunctionGraph(head.TopOut);
-            AddLink(head, head.TopOut, "top");
+            [XmlAttribute]
+            public string Id;
+            [XmlAttribute]
+            public string Label;
+
+            public Node(string id, string label)
+            {
+                this.Id = id;
+                this.Label = label;
+            }
         }
 
-        if (head.BottomOut != null)
+        public struct Link
         {
-            WalkFunctionGraph(head.BottomOut);
-            AddLink(head, head.BottomOut, "bottom");
+            [XmlAttribute]
+            public string Source;
+            [XmlAttribute]
+            public string Target;
+            [XmlAttribute]
+            public string Label;
+
+            public Link(string source, string target, string label)
+            {
+                this.Source = source;
+                this.Target = target;
+                this.Label = label;
+            }
         }
 
-        if (head.TopOut == null && head.BottomOut == null)
+        public List<Node> Nodes { get; protected set; }
+        public List<Link> Links { get; protected set; }
+
+        public GraphWriter()
         {
-            Node returnNode = new Node() { Label = "Return", Id = "return" + head.Id };
-            AddNode(returnNode);
-            AddLink(new Link { Source = head.Id.ToString(), Target = returnNode.Id, Label = "return" });
+            Nodes = new List<Node>();
+            Links = new List<Link>();
         }
-    }
 
-    private void AddNode(FunctionNode n)
-    {
-        AddNode(new Node { Id = n.Id.ToString(), Label = n.ToString() });
-    }
+        public void AddNode(Node n)
+        {
+            this.Nodes.Add(n);
+        }
 
-    private void AddLink(FunctionNode a, FunctionNode b, string type)
-    {
-        AddLink(new Link { Source = a.Id.ToString(), Target = b.Id.ToString(), Label = type });
-    }
+        public void AddLink(Link l)
+        {
+            this.Links.Add(l);
+        }
 
-    public void Serialize(string xmlpath)
-    {
-        Graph g = new Graph();
-        g.Nodes = this.Nodes.ToArray();
-        g.Links = this.Links.ToArray();
 
-        XmlRootAttribute root = new XmlRootAttribute("DirectedGraph");
-        root.Namespace = "http://schemas.microsoft.com/vs/2009/dgml";
-        XmlSerializer serializer = new XmlSerializer(typeof(Graph), root);
-        XmlWriterSettings settings = new XmlWriterSettings();
-        settings.Indent = true;
-        using (XmlWriter xmlWriter = XmlWriter.Create(xmlpath, settings))
-            serializer.Serialize(xmlWriter, g);
+        private HashSet<int> _seen = new HashSet<int>();
+        public void WalkFunctionGraph(FunctionNode head)
+        {
+            if (!_seen.Contains(head.Id))
+            {
+                AddNode(head);
+                _seen.Add(head.Id);
+            }
+            else
+            {
+                return;
+            }
+
+            if (head.TopOut != null)
+            {
+                WalkFunctionGraph(head.TopOut);
+                AddLink(head, head.TopOut, "top");
+            }
+
+            if (head.BottomOut != null)
+            {
+                WalkFunctionGraph(head.BottomOut);
+                AddLink(head, head.BottomOut, "bottom");
+            }
+
+            if (head.TopOut == null && head.BottomOut == null)
+            {
+                Node returnNode = new Node() { Label = "Return", Id = "return" + head.Id };
+                AddNode(returnNode);
+                AddLink(new Link { Source = head.Id.ToString(), Target = returnNode.Id, Label = "return" });
+            }
+        }
+
+        private void AddNode(FunctionNode n)
+        {
+            AddNode(new Node { Id = n.Id.ToString(), Label = n.ToString() });
+        }
+
+        private void AddLink(FunctionNode a, FunctionNode b, string type)
+        {
+            AddLink(new Link { Source = a.Id.ToString(), Target = b.Id.ToString(), Label = type });
+        }
+
+        public void Serialize(string xmlpath)
+        {
+            Graph g = new Graph();
+            g.Nodes = this.Nodes.ToArray();
+            g.Links = this.Links.ToArray();
+
+            XmlRootAttribute root = new XmlRootAttribute("DirectedGraph");
+            root.Namespace = "http://schemas.microsoft.com/vs/2009/dgml";
+            XmlSerializer serializer = new XmlSerializer(typeof(Graph), root);
+            XmlWriterSettings settings = new XmlWriterSettings();
+            settings.Indent = true;
+            using (XmlWriter xmlWriter = XmlWriter.Create(xmlpath, settings))
+                serializer.Serialize(xmlWriter, g);
+        }
     }
 }
