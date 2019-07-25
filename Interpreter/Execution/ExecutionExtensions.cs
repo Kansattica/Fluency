a972fb0bde026623c1fb9bd06d49b008dfd56b60
Grace H <grace@hypnovir.us>
2019-07-24 18:29:46 -0700

    Add (empty) execution extensions and tests.

---
+++ Interpreter/Execution/ExecutionExtensions.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Fluency.Interpreter.Execution.Extensions
+{
+    /// <summary>
+    /// Extension methods to help execution.
+    /// </summary>
+    public static class ExecutionExtensions
+    {
+    }
+}
---
+++ Tests/ExecutionTests/ExecutionExtensionTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fluency.Interpreter.Execution.Extensions;
+using System.Collections.Generic;
+
+namespace Fluency.Tests.Execution
+{
+    [TestClass]
+    public class ExecutionExtensionTests
+    {
+    }
+}
