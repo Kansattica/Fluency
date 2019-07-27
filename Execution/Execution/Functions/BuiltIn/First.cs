e5ec5e9a64bb06a5f779eb12a67e73559a810466
Grace H <grace@hypnovir.us>
2019-07-27 01:18:32 -0700

    Two working examples of recursion!

--- Execution/Execution/Functions/BuiltIn/First.cs
+++ Execution/Execution/Functions/BuiltIn/First.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Fluency.Common;
 using Fluency.Execution.Exceptions;
@@ -7,9 +8,9 @@ namespace Fluency.Execution.Functions.BuiltIn
 {
 
     /// <summary>
-    /// Returns the first N values, then stops.
+    /// Returns the first N values, then puts the rest on the bottom.
     /// </summary>
-    public class First : ITopIn, ITopOut
+    public class First : ITopIn, ITopOut, IBottomOut
     {
 
         public virtual string Name => nameof(First);
@@ -27,6 +28,27 @@ namespace Fluency.Execution.Functions.BuiltIn
             {
                 throw new ExecutionException("First takes either zero or one argument.");
             }
+        }
+
+        private Queue<Value> topQueue = new Queue<Value>();
+        public Value Bottom()
+        {
+            Value value = null;
+
+            // If we haven't sent enough off the top yet, queue that many up for the top
+            while (toAllow > 0 && (value = TopInput()))
+            {
+                toAllow--;
+                topQueue.Enqueue(value);
+            }
+
+            //didn't get a value up there or didn't have to
+            if (value == null)
+            {
+                return TopInput();
+            }
+
+            return value; //only if the while loop set it to done
         }
 
         public Value Top()
