659da8f094963581051bdaf67ba176740812f3f2
Grace H <grace@hypnovir.us>
2019-07-28 14:57:08 -0700

    Implement arguments for user-defined functions.

--- Execution/Execution/Functions/UserDefinedFunction.cs
+++ Execution/Execution/Functions/UserDefinedFunction.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
 using System.Linq;
+using Fluency.Common;
+using Fluency.Execution.Exceptions;
 using Fluency.Execution.Extensions;
-using Fluency.Execution.Parsing.Entities;
+using Fluency.Execution.Parsing.Entities.ArgumentTypes;
 using Fluency.Execution.Parsing.Entities.FunctionGraph;
 
 namespace Fluency.Execution.Functions
@@ -9,10 +11,10 @@ namespace Fluency.Execution.Functions
     public class UserDefinedFunction : IFunction, ITopIn, ITopOut, IBottomIn, IBottomOut
     {
         public string Name { get; private set; }
-        public Argument[] Arguments { get; private set; }
+        public FunctionArg[] Arguments { get; private set; }
 
 
-        public GetNext TopInput { set => _topHead.TypedFunction.TopInput = value; }
+        public GetNext TopInput { set => _topHead.TypedFunction.TopInput = WrapInput(value); }
         public GetNext BottomInput { set => _bottomHead.TypedFunction.TopInput = value; }
 
         private ExecutableNode<ITopIn> _topHead;
@@ -25,6 +27,7 @@ namespace Fluency.Execution.Functions
 
         private ITopOut _topTailOut;
         private ITopOut _bottomTailOut;
+
         public Value Top()
         {
             return _topTailOut.Top();
@@ -35,12 +38,63 @@ namespace Fluency.Execution.Functions
             return _bottomTailOut.Top();
         }
 
+        //idea for runtime arguments:
+        //if you got arguments in the constructor, store them, for the first [number of arguments given], do the function on them instead
+        // probably hook into TopInput for this
+        // also validate the arguments in the ctor, make sure the types match up
+        // we should also validate incoming args too, of course
+        private Value[] _runtimeArgs;
+        private GetNext WrapInput(GetNext topinput)
+        {
+            if (Arguments.Length == 0 || _runtimeArgs.Length == 0 || Arguments.Any(x => x.Type == FluencyType.Function && x.GetAs<string>() == "..."))
+                return topinput;
+            
+            if (_runtimeArgs.Length > Arguments.Length)
+                throw new ExecutionException("Too many arguments passed to {0}. Expected {1}, got {2}.", Name, Arguments.Length, _runtimeArgs.Length);
+            
+            var argumentsValid = Arguments.Zip(_runtimeArgs, (a, v) => (a, v, valid: ValidateArgument(a, v))).Where(x => !x.valid)
+                .Aggregate("", 
+                (acc, curr) => $"Invalid argument. Function {Name} expects an argument of type {curr.a.Type} and got {curr.v.ToString()}, which is of type {curr.v.Type}.\n" + acc);
+
+            if (!string.IsNullOrWhiteSpace(argumentsValid))
+                throw new ExecutionException(argumentsValid);
+
+            int argIndex = 0;
+            return () =>
+            {
+                if (argIndex < _runtimeArgs.Length)
+                    return _runtimeArgs[argIndex++];
+                else if (argIndex == Arguments.Length)
+                    return Value.Finished;
+                else
+                {
+                    Value v = topinput();
+                    if (!ValidateArgument(Arguments[argIndex++], v))
+                        throw new ExecutionException("Function {0} tried to take value {1} (type {2}) from the pipeline, when it declares type {3}.", 
+                            Name, v, v.Type, Arguments[argIndex].DeclaredType);
+
+                    return v;
+                }
+            };
+
+        }
+
+        private bool ValidateArgument(FunctionArg argument, Value value)
+        {
+            if (argument.DeclaredType == FluencyType.Any)
+                return true;
+            
+            return argument.DeclaredType == value.Type;
+        }
+
         public UserDefinedFunction(FunctionGraph graph, Value[] arguments, IFunctionResolver linker)
         {
             Name = graph.Name;
             Arguments = graph.Arguments;
+            _runtimeArgs = arguments;
             Expand(graph.Head, linker);
         }
+
         private void Expand(FunctionNode head, IFunctionResolver linker)
         {
             //Basically, we want to walk the FunctionGraph and turn it into an ExecutableGraph of IFunctions.
