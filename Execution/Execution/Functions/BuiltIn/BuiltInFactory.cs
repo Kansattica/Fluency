7f2c3488d81883deb76d2e647e72877a1f22a4ba
Grace H <grace@hypnovir.us>
2019-07-30 17:33:01 -0700

    Implement mult, parse, and div double. Really gotta do numerics.

--- Execution/Execution/Functions/BuiltIn/BuiltInFactory.cs
+++ Execution/Execution/Functions/BuiltIn/BuiltInFactory.cs
@@ -34,22 +34,25 @@ namespace Fluency.Execution.Functions.BuiltIn.Factory
             {"Drain", (_) => new Drain()},
             {"Add", (args) => new WrapBinary<int, int, int>((a, b) => a + b, FluencyType.Int, FluencyType.Int,"Add", args )},
             {"Mult", (args) => new WrapBinary<int, int, int>((a, b) => a * b, FluencyType.Int, FluencyType.Int,"Mult", args )},
+            {"MultDouble", (args) => new WrapBinary<double, double, double>((a, b) => a * b, FluencyType.Double, FluencyType.Double,"MultDouble", args )},
             {"Sqrt", (_) => new WrapUnary<int, double>(a => Math.Sqrt(a), FluencyType.Double, "Sqrt")},
             {"Floor", (_) => new WrapUnary<double, int>(a => (int)a, FluencyType.Int, "Floor")},
             {"Equals", (args) => new WrapBinary<object, object, bool>((a, b) => a.Equals(b) , FluencyType.Any, FluencyType.Bool, "Equals", args)},
             {"LessThan", (args) => new WrapBinary<int, int, bool>((a, b) => a < b, FluencyType.Int, FluencyType.Bool, "LessThan", args)},
             {"GreaterThan", (args) => new WrapBinary<int, int, bool>((a, b) => a > b, FluencyType.Int, FluencyType.Bool, "GreaterThan", args)},
             {"And", (_) => new WrapBinary<bool, bool, bool>((a, b) => a && b, FluencyType.Bool, FluencyType.Bool, "And", _emptyArgs)},
-            {"All", (args) => new WrapBinaryFold<bool>((a, b) => a && b, FluencyType.Bool, "All", args, _bools[0])}, 
+            {"All", (args) => new WrapBinaryFold<bool>((a, b) => a && b, FluencyType.Bool, "All", args, _bools[0])},
             {"Or", (_) => new WrapBinary<bool, bool, bool>((a, b) => a || b, FluencyType.Bool, FluencyType.Bool, "Or", _emptyArgs)},
-            {"Any", (args) => new WrapBinaryFold<bool>((a, b) => a || b, FluencyType.Bool, "Any", args, _bools[1])}, 
+            {"Any", (args) => new WrapBinaryFold<bool>((a, b) => a || b, FluencyType.Bool, "Any", args, _bools[1])},
             {"Not", (_) => new WrapUnary<bool, bool>(a => !a, FluencyType.Bool, "Not")},
             {"AddDouble", (args) => new WrapBinary<double, double, double>((a, b) => a + b, FluencyType.Double, FluencyType.Double, "AddDouble", args )},
             {"ParseInt", (_) => new WrapUnary<string, int>(int.Parse, FluencyType.Int, "ParseInt")},
+            {"ParseDouble", (_) => new WrapUnary<string, double>(double.Parse, FluencyType.Double, "ParseDouble")},
             {"Concat", (args) => new WrapBinary<object, object, object>((a, b) => a.ToString() + b.ToString(), FluencyType.Any, FluencyType.String, "Concat", args )},
             {"Split", (args) => new WrapBinaryStreamOutput<string, string, string>((a, b) =>
                                              a.Split(new string[] {b}, StringSplitOptions.RemoveEmptyEntries), FluencyType.String, FluencyType.String, "Split", args )},
             {"DivMod", (args) => new WrapBinaryTwoOutputs<int, int, int, int>((a, b) => (a/b, a%b) , FluencyType.Int, FluencyType.Int, FluencyType.Int, "DivMod", args )},
+            {"DivDouble", (args) => new WrapBinary<double, double, double>((a, b) => a/b , FluencyType.Double, FluencyType.Double, "DivDouble", args )},
         };
 
         public IFunction Resolve(string name, Value[] arguments)
